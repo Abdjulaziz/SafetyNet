c532123f643fe06c3615b4621340b878b9aeacfa
abdjulaziz <abdulazizpriv@gmail.com>
2022-12-20 18:02:16 +0100

    fils added

---
+++ navigation/MainContainer.js
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+// Screens
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+
+//Screen names
+const homeTab = "Home";
+const profileTab = "Details";
+const settingsTab = "Settings";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeTab}
+        screenOptions={({ route }) => ({
+          title: "",
+          tabBarActiveTintColor: "#15921F",
+          tabBarInactiveTintColor: "#000",
+          tabBarStyle: { display: "flex" },
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+            size = 30;
+            if (rn === homeTab) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === profileTab) {
+              iconName = focused ? "person-sharp" : "person-outline";
+            } else if (rn === settingsTab) {
+              iconName = focused ? "options-sharp" : "options-outline";
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name={homeTab} component={HomeScreen} />
+        <Tab.Screen name={profileTab} component={ProfileScreen} />
+        <Tab.Screen name={settingsTab} component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
