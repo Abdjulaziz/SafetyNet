69cf0bc4ad3894df6b385bdc1abd1a493b66cf33
MarkfromDK <mark4023@edu.ucl.dk>
2022-12-22 19:19:34 +0100

    profilbubble tilføjet som component

---
+++ navigation/components/ProfileBubble.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Dimensions, TouchableHighlight, Text, StyleSheet } from 'react-native';
+
+export default function Circle() {
+  return (
+    <TouchableHighlight
+      style = {{
+        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+        width: Dimensions.get('window').width * 0.5,
+        height: Dimensions.get('window').width * 0.5,
+        backgroundColor:'#fbbc05',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 70,
+      }}
+      underlayColor = '#ccc'
+      onPress = { () => alert('Yaay!') }
+    >
+      <Text style={styles.titleText}>J</Text>
+    </TouchableHighlight>
+    )
+  }
+  
+  const styles = StyleSheet.create({
+    titleText: {
+      fontSize: 50,
+      fontWeight: 'bold',
+    }
+  })
