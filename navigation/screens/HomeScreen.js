60bec16961e9a28746af83270167f3cb32399cee
abdjulaziz <abdulazizpriv@gmail.com>
2022-12-29 10:26:34 +0100

    registration and log out done

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,15 +1,53 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { firebase } from "../../config";
 
 export default function HomeScreen({ navigation }) {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setName(snapshot.data());
+        } else {
+          console.log("User does not exist");
+        }
+      });
+  }, []);
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => alert('This is the "Home" screen.')}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Home Screen
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+        hello, {name.firstName}
       </Text>
-    </View>
+      <TouchableOpacity
+        onPress={() => firebase.auth().signOut()}
+        style={styles.button}
+      >
+        <Text style={{ fontSize: 22, fontWeight: "bold" }}>Sign out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+  },
+  button: {
+    marginTop: 50,
+    height: 70,
+    width: 250,
+    backgroundColor: "#026efd",
+    alignItems: "center",
+    alignContent: "center",
+    borderRadius: 50,
+  },
+});
